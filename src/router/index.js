e4713de7bb7f24b85b0f4047dd659025989b68e7
avazbek <avazbekjumanazarov06@gmail.com>
2023-07-10 15:51:33 +0500

    aa

--- src/router/index.js
+++ src/router/index.js
@@ -55,6 +55,11 @@ const router = createRouter({
       component: () => import("../views/dorixonaadd.vue"),
     },
     {
+      path: "/tick",
+      name: "tick",
+      component: () => import("../views/tick.vue"),
+    },
+    {
       path: "/shahsiydorixona",
       name: "shahsiydorixona",
       component: () => import("../views/shahsiydorixona.vue"),
---
+++ src/views/tick.vue
@@ -0,0 +1,66 @@
+<template>
+  <div as="nav" class="bg-gray-800">
+    <div class="mx-auto sm:px-6">
+      <div class="relative flex h-16 items-center justify-between">
+        <div
+          class="flex flex-1 items-center m-1 sm:items-stretch sm:justify-start"
+        >
+          <div class="flex flex-shrink-0 items-center">
+            <RouterLink to="/shahsiydorixona"
+              ><h1 class="text-2xl text-white ml-2">Tabriklaymiz</h1></RouterLink
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div  class="flex justify-center mt-64"   >
+    <svg
+    width="115px"
+    height="115px"
+    viewBox="0 0 133 133"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    class=""
+>
+    <g
+        id="check-group"
+        stroke="none"
+        stroke-width="1"
+        fill="none"
+        fill-rule="evenodd"
+    >
+            <circle
+            id="filled-circle"
+            fill="#07b481"
+            cx="66.5"
+            cy="66.5"
+            r="54.5"
+        />
+        <circle
+            id="white-circle"
+            fill="#FFFFFF"
+            cx="66.5"
+            cy="66.5"
+            r="55.5"
+        />
+        <circle
+            id="outline"
+            stroke="#07b481"
+            stroke-width="4"
+            cx="66.5"
+            cy="66.5"
+            r="54.5"
+        />
+        <polyline
+            id="check"
+            stroke="#FFFFFF"
+            stroke-width="5.5"
+            points="41 70 56 85 92 49"
+        />
+    </g>
+</svg>
+</div><div class="flex justify-center text-2xl font-bold">
+<p>Tabriklaymiz! Siz muvafaqiyattlinavbat oldingiz. </p></div>
+</template>
